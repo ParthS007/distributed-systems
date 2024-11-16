dd1fa202e87f9a0cb34fbca7b5a37bbbbde90247
Parth Shandilya <24358501+ParthS007@users.noreply.github.com>
2024-11-16 19:33:46 +0100

    hpc: fix fibonacci parallel

--- hpc/2024/Ex3/code-skeletons/fibonacci.c
+++ hpc/2024/Ex3/code-skeletons/fibonacci.c
@@ -6,32 +6,46 @@
 #include <ctype.h>
 
 
-int fibonacci(int n) {
+int fibonacci_seq(int n) {
     if (n <= 1) {
         return n;
     }
-    return fibonacci(n-1) + fibonacci(n-2);
+    return fibonacci_seq(n-1) + fibonacci_seq(n-2);
 }
 
 void sequentialFibonacci(int terms) {
     printf("Sequential Fibonacci:\n");
     for (int i = 0; i < terms; i++) {
-        printf("%d, ", fibonacci(i));
+        printf("%d, ", fibonacci_seq(i));
     }
     printf("\n");
 }
 
-void parallelFibonacci(int terms) {
-    int fib[terms];
-    printf("Parallel Fibonacci:\n");
-
-    #pragma omp parallel for
-    for (int i = 0; i < terms; i++) {
-        fib[i] = fibonacci(i);
+int fibonacci_par(int n) {
+    int i, j;
+    if (n < 2)
+        return n;
+    else {
+        #pragma omp task shared(i)
+        i = fibonacci_par(n-1);
+        #pragma omp task shared(j)
+        j = fibonacci_par(n-2);
+        #pragma omp taskwait
+        return i + j;
     }
+}
 
-    for (int i = 0; i < terms; i++) {
-        printf("%d, ", fib[i]);
+void parallelFibonacci(int terms) {
+    printf("Parallel Fibonacci:\n");
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            for (int i = 0; i < terms; i++) {
+                int result = fibonacci_par(i);
+                printf("%d, ", result);
+            }
+        }
     }
     printf("\n");
 }
